9b0a126914c54833d4a2a8fa83e51e895af80359
Josh Endter <jendter@users.noreply.github.com>
2015-06-23 10:34:19 -0400

    Scroll View should work with paging.

--- ScrollViews/ViewController.m
+++ ScrollViews/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) UIView *rootScrollView;
+
 @end
 
 @implementation ViewController
@@ -17,6 +20,41 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.rootScrollView = [[UIView alloc] init];
+    [self.scrollView addSubview:self.rootScrollView];
+    
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    NSArray *imageNames = @[@"Lighthouse-in-Field", @"Lighthouse-night", @"Lighthouse-zoomed"];
+    
+    int xPosition = 0;
+    
+    for (NSString *imageName in imageNames) {
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:(CGRect){xPosition,0,self.scrollView.frame.size.width, self.scrollView.frame.size.height}];
+        imageView.contentMode = UIViewContentModeScaleAspectFit;
+        UIImage *image = [UIImage imageNamed:imageName];
+        imageView.image = image;
+        [self.rootScrollView addSubview:imageView];
+        xPosition = xPosition + self.scrollView.frame.size.width;
+    }
+    
+    
+    
+    //    int xPosition = 0;
+    //
+    //    for (NSString *imageName in imageNames) {
+    //        UIImageView *imageView = [[UIImageView alloc] initWithFrame:(CGRect){xPosition,0,self.scrollView.frame.size.width, self.scrollView.frame.size.height}];
+    //        UIImage *image = [UIImage imageNamed:imageName];
+    //        imageView.image = image;
+    //        [self.scrollView addSubview:imageView];
+    //        xPosition = xPosition + self.scrollView.frame.size.width;
+    //    }
+    
+    self.scrollView.contentSize = (CGSize){xPosition, self.scrollView.frame.size.height};
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
