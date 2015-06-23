d27fa6f416fdca9e1bdf6639a48d82e697c1ff76
Josh Endter <jendter@users.noreply.github.com>
2015-06-23 18:10:46 -0400

    Comment cleaning

--- ScrollViews/ViewController.m
+++ ScrollViews/ViewController.m
@@ -26,24 +26,15 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    
-    //[self.navigationController setNavigationBarHidden:YES animated:NO];
-    
-    
 }
 
 - (void)viewDidLayoutSubviews {
-    //[super viewDidLayoutSubviews];
-    
-    //NSLog(@"The scroll view starts out as origin y: %f", self.scrollView.frame.origin.y);
-    //NSLog(@"The scroll view starts out as origin y: %f", self.scrollView.frame.origin.y);
     
     self.rootScrollView = [[UIView alloc] init];
     [self.scrollView addSubview:self.rootScrollView];
     
     UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc]
                                              initWithTarget:self action:@selector(handleImageTap:)];
-    //tapRecognizer.delegate = self;
     [self.scrollView addGestureRecognizer:tapRecognizer];
     
     self.imageNames = @[@"Lighthouse-in-Field", @"Lighthouse-night", @"Lighthouse-zoomed"];
@@ -52,7 +43,6 @@
     
     for (NSString *imageName in self.imageNames) {
         UIImageView *imageView = [[UIImageView alloc] initWithFrame:(CGRect){xPosition,0,self.scrollView.frame.size.width, self.scrollView.frame.size.height}];
-        //NSLog(@"Image Frame height:%f", imageView.frame.size.height);
         imageView.contentMode = UIViewContentModeScaleAspectFit;
         UIImage *image = [UIImage imageNamed:imageName];
         imageView.image = image;
@@ -78,11 +68,9 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([segue.identifier isEqual:@"showImageDetailView"]) {
         UIScrollView *scrollView = (UIScrollView *)sender;
-        NSLog(@"Recognizer X:%f, Recognizer Y:%f", scrollView.contentOffset.x, scrollView.contentOffset.y);
+        //NSLog(@"Recognizer X:%f, Recognizer Y:%f", scrollView.contentOffset.x, scrollView.contentOffset.y);
         
         int imageNum = scrollView.contentOffset.x / scrollView.frame.size.width;
-        
-        NSLog(@"Image num: %d", imageNum);
         
         ImageDetailViewController *imageDetail = (ImageDetailViewController *)segue.destinationViewController;
         
@@ -91,18 +79,9 @@
     }
 }
 
-- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
-    NSLog(@"ended scrolling");
-    
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
     int imageNum = scrollView.contentOffset.x/ scrollView.frame.size.width;
-    
     self.pageControl.currentPage = imageNum;
-    
-    
-    NSLog(@"Scroll Frame height:%f", self.scrollView.frame.size.height);
-    //imageDetail.imageName = self.imageNames[imageNum];
-
-    
 }
 
 
